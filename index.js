a3ede251ce69006928fc34be9843f7f19456e0ef
Jonas-Kunz <jonas_K_7@outlook.com>
2024-01-18 21:25:36 -0400

    made some stuff

---
+++ index.js
@@ -0,0 +1,39 @@
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
+
+// fetch ip test
+// fetchMyIP((error, ip, status) => {
+//   if(error) {
+//     console.log("It Didn't Work! ", error)
+//     console.log("Status: ", status);
+//     return "It Didn't Work! ", error
+//   }
+//   console.log("It Worked! Returned IP: ", ip, )
+//   console.log("Status: ", status);
+// });
+
+// fetchCoordsByIP("173.252.55.11", (error, locationObj, status) => {
+//   if (error) {
+//     console.log(
+//       `It Didn't Work! Error: Success status was ${error.success}. Message was: ${error.message} `
+//     );
+//     console.log("Status: ", status);
+//     return error;
+//   } else {
+//     console.log(locationObj);
+//     return locationObj;
+//   }
+// });
+
+// fetchISSFlyOverTimes(
+//   { latitude: "43.8374576", longitude: "43.8374576" },
+//   (error, passes) => {
+//     if (error) {
+//       console.log("ZADDY HELP ME: ", error);
+//       return error
+//     } else {
+//       console.log("---YAAAAS SLAAAAY---");
+//       console.log("Passes: ", passes)
+//       return true
+//     }
+//   }
+// );
---
+++ iss.js
@@ -0,0 +1,76 @@
+const request = require("request");
+
+const fetchMyIP = function (callback) {
+  request("https://api.ipify.org?format=json", (error, response, body) => {
+    //console.log("ERROR: ", error);
+    //console.log("StatusCode: ", response.statusCode);
+    //console.log("BODY: ", body);
+    if (error) {
+      return callback(error, null, response.statusCode);
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null, response.statusCode);
+      return;
+    }
+
+    const ip = JSON.parse(body).ip;
+
+    callback(null, ip, response.statusCode);
+  });
+};
+
+const fetchCoordsByIP = function (ip, callback) {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null, response.statusCode);
+    }
+
+    const data = JSON.parse(body);
+    if (!data.success) {
+      callback(data, null, response.statusCode);
+      return;
+    }
+
+    callback(
+      null,
+      { latitude: `${data.latitude}`, longitude: `${data.latitude}` },
+      response.statusCode
+    );
+  });
+};
+
+const fetchISSFlyOverTimes = function (locationobj, callback) {
+  request(
+    `https://iss-flyover.herokuapp.com/json/?lat=${locationobj.latitude}&lon=${locationobj.longitude}`,
+    (error, response, body) => {
+      //console.log("error: ", error);
+      //console.log("response: ", response);
+      //console.log("body: ", body)
+      console.log(response.statusCode)
+
+      if (body === "invalid coordinates") {
+        callback("invalid coordinates", null , response.statusCode)
+        return "invalid coordinates"
+      }
+      if (error) {
+        callback(error, null, response.statusCode);
+        return;
+      }
+      //this does nothing in any oth these examples becasue with an invalid url it still returns a code
+      if (response.statusCode !== 200) {
+        const msg = `Status Code ${response.statusCode} when fetching ISS pass time: ${body}`;
+        callback(Error(msg), null, response.statusCode);
+        return;
+      }
+
+      const data = JSON.parse(body).response;
+      //console.log("DATA: ", data.response);
+
+      callback(null, data, response.statusCode);
+    }
+  );
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
